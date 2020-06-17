c8201894d4cf7df0f2ff824c094281aa21872086
ledgerofzee <48485895+ledgerofzee@users.noreply.github.com>
2020-06-17 13:17:25 +0800

    Update MachineMind.py

--- MachineMind.py
+++ MachineMind.py
@@ -1,6 +1,6 @@
 """The following variables are the base variables:
     baseSheets, freSheets, baseVern, nexVern, backTrackProb, creaProb, fallToProb,
-    pingOnto, revealText, containFlu, tickleRex
+    pingOnto, revealShow, blockIntent, containFlu, tickleRex, charmOnx
     
     pingOnto is a deterministic variable which causes the neural network to zone in onto a new input.
 """
